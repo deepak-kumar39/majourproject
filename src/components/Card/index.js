e86f15038f2ba0672e5faff944702b7b333e8703
deepak-kumar39 <deepakkumarrr39@gmail.com>
2021-03-01 16:59:12 +0530

    services update

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -9,8 +9,7 @@ function Card(props) {
           <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+             {props.maintext}
             </p>
             <Link to="/" className="btn btn-dark">
               {props.btntxt}
--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -10,16 +10,15 @@ function Services() {
   return (
     
     <section className="service-section">
-      <div className="my-5">
-      </div>
+      
       <div className="container-fluid mb-5">
         <div className="row">
           <div className="col-10 mx-auto">
             <div className="row">
-             <Card image = {heavy} btntxt = "Book Now" title ="Heavy luggage" />
-              <Card image={ship} btntxt = "Book Now" title ="Transport via Ship"/>
-              <Card image={small} btntxt = "Book Now" title ="Small luggage"/>
-              <Card image={train} btntxt = "Book Now" title ="Via train"/>
+             <Card image = {heavy} btntxt = "Book Now" title ="Transfer by Road" maintext="To send luggage within states book this service"/>
+              <Card image={ship} btntxt = "Book Now" title ="Transfer via Ship" maintext="To send luggage via ship book this service"/>
+              <Card image={small} btntxt = "Book Now" title ="Within Town" maintext="To send luggage within town book this service"/>
+              <Card image={train} btntxt = "Book Now" title =" Transfer Via train" maintext="To send luggage via train book this service"/>
             </div>
           </div>
         </div>
